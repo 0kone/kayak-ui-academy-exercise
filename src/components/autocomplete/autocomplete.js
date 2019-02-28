5e96dfa88ac337ddf1b899f7baa80c5afd209a24
0kone <32953866+0kone@users.noreply.github.com>
2019-02-28 22:56:57 +0200

    Finished exercise

--- src/components/autocomplete/autocomplete.js
+++ src/components/autocomplete/autocomplete.js
@@ -1,11 +1,75 @@
-import React from 'react';
-
+import React, { Component } from 'react';
 import styles from './autocomplete.css';
 
-const Autocomplete = () => (
-  <div className={styles.container}>
-    <input placeholder="Search." />
-  </div>
-);
+class Autocomplete extends Component {
+  state = {
+    query: '',
+    results: []
+  };
 
+  getInfo() {
+    if (this.state.query.length >= 3) {
+      fetch(
+        `https://api.themoviedb.org/3/search/movie?api_key=cab2afe8b43cf5386e374c47aeef4fca&language=en-US&query=${
+          this.state.query
+        }&page=1&include_adult=false`
+      )
+        .then(results => results.json())
+        .then(result => {
+          this.setState({ results: result.results.splice(0, 8) });
+        });
+    }
+  }
+  clearInfo = () => {
+    this.setState(() => ({ results: [] }));
+  };
+
+  handleInputChange = () => {
+    this.setState(
+      {
+        query: this.search.value
+      },
+      () => {
+        if (this.state.query && this.state.query.length > 1) {
+          this.getInfo();
+        } else if (!this.state.query) {
+          this.clearInfo();
+        }
+      }
+    );
+  };
+
+  suggestionSelect(title) {
+    this.setState({
+      query: title,
+      results: []
+    });
+  }
+
+  Suggestions() {
+    var options = this.state.results.map(r => (
+      <li onClick={() => this.suggestionSelect(r.original_title)} key={r.id}>
+        {r.original_title}
+        <p>{r.vote_average + ' Rating, ' + r.release_date.slice(0, 4)} </p>
+      </li>
+    ));
+    return <ul>{options}</ul>;
+  }
+
+  render() {
+    return (
+      <form>
+        <div className="autocomplete">
+          <input
+            placeholder="Enter movie name"
+            ref={input => (this.search = input)}
+            value={this.state.query}
+            onChange={this.handleInputChange}
+          />
+          {this.Suggestions()}
+        </div>
+      </form>
+    );
+  }
+}
 export default Autocomplete;
